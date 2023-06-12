068c03a71428824d2c53b0f00ab889eed8ebb08c
Bill Rose <wwriv1991@gmail.com>
2023-06-11 20:10:45 -0400

    Switch to a lookup table for every pixel

--- EF_TOTEM_Phase5/color_wipe.ino
+++ EF_TOTEM_Phase5/color_wipe.ino
@@ -1,20 +1,8 @@
-
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  uint16_t i, j, p, y;
-
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    if(i<240){
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(0);
-    }
-      else {
-        for (y=0; y<9; y++){
-          for (p=0; p<33; p++){
-            strip.setPixelColor(row_array[y][p], c);
-            strip.show();
-            delay(0);
-      }}}
+void colorWipe(uint32_t color, uint8_t wait) {
+  for (uint16_t pixel = 0; pixel < strip.numPixels(); pixel++) {
+    strip.setPixelColor(ordered_pixels[pixel], color);
+    strip.show();
+    delay(0);
   }
 }
